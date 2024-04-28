686ec8e74a58cd491a9fcd4836878f6b110badca
Prado Nicolas <nicolas88prado@gmail.com>
2024-04-28 16:51:14 -0300

    realice el maquetado necesario para la web

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
-
+import { Container} from "react-bootstrap"
 
 function App() {
 
   return (
     <>
-      
+      <Container>
+        <h1>Hellow  World!</h1>
+      </Container>
     </>
   )
 }
